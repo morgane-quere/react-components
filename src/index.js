243235d8b085e974e9cac847c5110a30c030755b
morgane quere <qu.morgane@gmail.com>
2021-03-31 20:45:15 +0200

    passed author, content and avatar props

--- src/index.js
+++ src/index.js
@@ -6,9 +6,24 @@ import CommentDetail from './CommentDetail';
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Bob" />
-      <CommentDetail author="Bobette" />
-      <CommentDetail author="Toto" />
+      <CommentDetail 
+        author="Bob" 
+        timeAgo="Today at 4:45PM" 
+        content="Awesome post!" 
+        avatar={faker.image.avatar()} 
+      />
+      <CommentDetail 
+        author="Bobette" 
+        timeAgo="Today at 6:14PM" 
+        content="Amazing post!" 
+        avatar={faker.image.avatar()} 
+      />
+      <CommentDetail 
+        author="Toto" 
+        timeAgo="Today at 6:35PM" 
+        content="I love this blog!" 
+        avatar={faker.image.avatar()} 
+      />
     </div>
   );
 };
