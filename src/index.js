7a7f86269d154afc3a5883b9948a7d49f796ece0
morgane quere <qu.morgane@gmail.com>
2021-04-01 09:58:11 +0200

    created approval card

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+
 
 const App = () => {
   return (
     <div className="ui container comments">
+      <ApprovalCard />
       <CommentDetail 
         author="Bob" 
         timeAgo="Today at 4:45PM" 
